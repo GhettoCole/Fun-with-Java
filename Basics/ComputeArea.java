535cd51212b49dd23225296744b6af19d046d946
Given Lepita <given.lepita@gmail.com>
2024-01-10 16:49:20 +0200

    Simple Circle's Area Calculation

---
+++ Basics/ComputeArea.java
@@ -0,0 +1,11 @@
+/*
+A program to calculate the radius of a circle
+*/
+
+public class ComputeArea {
+    public static void main(String[] args) {
+        double radius = 20;
+        double area = Math.PI * radius * radius;
+        System.out.println("The area of the circlen of radius" + radius + " is " + area);
+    }
+}
